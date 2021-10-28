225cde788ae997e68d489901c0791de761325374
Kyungjoo Ha <429why@gmail.com>
2021-10-28 13:01:19 +0900

    add: Myinfo controller

--- server/controllers/mypage/myinfo.js
+++ server/controllers/mypage/myinfo.js
@@ -1,4 +1,30 @@
+const { isAuthorized } = require('../tokenFunctions');
 const { users } = require('../../models');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
+  try {
+    const accessTokenData = isAuthorized(req);
+    if (!accessTokenData) {
+      return res.status(401).send({ message: 'You\'re not logged in.' });
+    } else {
+      const userInfo = await users.findOne({
+        where: {
+          id: accessTokenData.id
+        }
+      });
+
+      res.status(200).json({
+        data: {
+          nickname: userInfo.nickname,
+          email: userInfo.email
+        },
+        message: 'ok'
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      message: 'error'
+    });
+  }
 };
