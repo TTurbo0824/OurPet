8a15d60d2b9d3df1ae27de45e9bc0b447d78ff66
Kyungjoo Ha <429why@gmail.com>
2021-10-30 11:03:04 +0900

    fix: Redux actions

--- client/src/redux/action.js
+++ client/src/redux/action.js
@@ -1,5 +1,6 @@
 export const LOG_IN = 'LOG_IN';
 export const LOG_OUT = 'LOG_OUT';
+export const EDIT_INFO = 'EDIT_INFO';
 export const TOKEN_EXPIRED = 'TOKEN_EXPIRED';
 export const REQUEST_DOGWALKER = 'REQUEST_DOGWALKER';
 export const CANCEL_DOGWALKER = 'CANCEL_DOGWALKER';
@@ -12,6 +13,11 @@ export const userLogin = (token, loginData) => ({
 
 export const userLogout = () => ({
   type: LOG_OUT
+});
+
+export const editInfo = (userData) => ({
+  type: EDIT_INFO,
+  payload: userData
 });
 
 export const tokenExpired = () => ({
