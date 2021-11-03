2a53493d62f6933983d182aadfe3f75d2e6c61e3
Kyungjoo Ha <429why@gmail.com>
2021-11-03 13:23:21 +0900

    add: Histories controllers

--- server/controllers/history/rating.js
+++ server/controllers/history/rating.js
@@ -1,4 +1,37 @@
-const { histories } = require('../../models');
+const { ratings } = require('../../models');
+const { isAuthorized } = require('../tokenFunctions');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
+  try {
+    // JUST FOR TESTING PURPOSES
+    // console.log(req.headers.authorization);
+    const accessTokenData = { id: req.headers.authorization };
+    // const accessTokenData = isAuthorized(req);
+
+    if (!accessTokenData) {
+      return res.status(401).json({ message: 'You\'re not logged in' });
+    } else {
+      const { historyId, rating } = req.body;
+
+        const userRating = await ratings.findAll({
+          where: {
+            historyId: historyId
+          }
+        });
+        console.log(userRating);
+
+        if (userRating.length > 0) {
+          return res.status(409).json({ message: 'already gave the rating' });
+        } else {
+          await ratings.create({
+            historyId: historyId,
+            rating: rating
+          });
+  
+          return res.status(200).json({ message: 'ok' });
+        }
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'error' });
+  }
 };
--- server/controllers/history/review.js
+++ server/controllers/history/review.js
@@ -1,4 +1,35 @@
-const { histories } = require('../../models');
+const { reviews } = require('../../models');
+const { isAuthorized } = require('../tokenFunctions');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
+  try {
+    // JUST FOR TESTING PURPOSES
+    // console.log(req.headers.authorization);
+    const accessTokenData = { id: req.headers.authorization };
+    // const accessTokenData = isAuthorized(req);
+
+    if (!accessTokenData) {
+      return res.status(401).json({ message: 'You\'re not logged in' });
+    } else {
+      const { historyId, review } = req.body;
+        const userReview = await reviews.findAll({
+          where: {
+            historyId: historyId
+          }
+        });
+
+        if (userReview.length > 0) {
+          return res.status(409).json({ message: 'already wrote the review' });
+        } else {
+          await reviews.create({
+            historyId: historyId,
+            review: review
+          });
+  
+          return res.status(200).json({ message: 'ok' });
+        }
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'error' });
+  }
 };
