5ba644783372530df7d31a996c21416c62e0f2f3
Kyungjoo Ha <429why@gmail.com>
2021-11-03 13:35:58 +0900

    add: Mypage controllers

--- server/controllers/mypage/edit-my-info.js
+++ server/controllers/mypage/edit-my-info.js
@@ -1,4 +1,69 @@
+const { isAuthorized } = require('../tokenFunctions');
 const { users } = require('../../models');
+const crypto = require('crypto');
+const Sequelize = require('sequelize');
+require('sequelize-values')(Sequelize);
 
-module.exports = (req, res) => {
+// PATCH http://localhost:80/user-info
+module.exports = async (req, res) => {
+  try {
+    // JUST FOR TESTING PURPOSES
+    // console.log(req.headers.authorization);
+    const accessTokenData = { id: req.headers.authorization };
+    // const accessTokenData = isAuthorized(req);
+
+    if (!accessTokenData) {
+      return res.status(401).json({ message: 'You\'re not logged in' });
+    } else {
+      const { nickname, password } = req.body;
+      // console.log(nickname, password);
+
+      let userInfo = await users.findOne({
+        where: {
+          id: accessTokenData.id
+        }
+      });
+
+      userInfo = Sequelize.getValues(userInfo);
+
+      if (userInfo) {
+        let salt = userInfo.salt;
+        let encryptedPassword = userInfo.password;
+
+        if (password !== '') {
+          salt = crypto.randomBytes(64).toString('hex');
+          encryptedPassword = crypto
+            .pbkdf2Sync(password, salt, 9999, 64, 'sha512')
+            .toString('base64');
+        }
+
+        let changedNickname = nickname;
+
+        if (changedNickname === '' || userInfo.nickname === changedNickname) {
+          changedNickname = userInfo.nickname;
+        }
+
+        // console.log(changedNickname);
+        // console.log(encryptedPassword);
+
+        await users.update(
+          {
+            nickname: changedNickname,
+            salt: salt,
+            password: encryptedPassword
+          },
+          { where: { id: accessTokenData.id } }
+        );
+
+        res.status(200).json({
+          data: {
+            nickname: changedNickname
+          },
+          message: 'Information updated'
+        });
+      }
+    }
+  } catch {
+    res.status(400).json({ message: 'error' });
+  }
 };
--- server/controllers/mypage/withdrawal.js
+++ server/controllers/mypage/withdrawal.js
@@ -1,4 +1,41 @@
+const { isAuthorized } = require('../tokenFunctions');
 const { users } = require('../../models');
 
-module.exports = (req, res) => {
+// DELETE http://localhost:80/withdrawal
+module.exports = async (req, res) => {
+  try {
+    const accessTokenData = isAuthorized(req);
+
+    if (!accessTokenData) {
+      return res.status(401).send({ message: 'You\'re not logged in.' });
+    } else {
+      const userInfo = await users.findOne({
+        where: {
+          id: accessTokenData.id
+        }
+      });
+
+      if (userInfo) {
+        await users.destroy({
+          where: {
+            id: accessTokenData.id
+          }
+        });
+
+        res.setHeader('authorization', '');
+
+        res.status(200).json({
+          message: 'Successfully withdrawn'
+        });
+      } else {
+        res.status(400).json({
+          message: 'error'
+        });
+      }
+    }
+  } catch {
+    res.status(400).json({
+      message: 'error'
+    });
+  }
 };
