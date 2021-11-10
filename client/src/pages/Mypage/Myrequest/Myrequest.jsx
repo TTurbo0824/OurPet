9533f14d42747ff568b168cb365529310a23ba5f
Kyungjoo Ha <429why@gmail.com>
2021-11-11 01:26:07 +0900

    add: Basic contents

--- client/src/pages/Mypage/Myrequest/Myrequest.jsx
+++ client/src/pages/Mypage/Myrequest/Myrequest.jsx
@@ -1,13 +1,66 @@
 import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import TopNavigation from '../../../components/TopNavigation';
+import { Colors } from '../../../components/utils/_var';
 
 export const MyRequestWrapper = styled.div`
   .main {
     display: flex;
-    min-height: calc(100vh - 8.9rem);
+    min-height: calc(100vh - 10.9rem);
+    justify-content: center;
   }
-`;
+  .container {
+    margin: 1rem auto;
+  }
+  .card {
+    display: grid;
+    grid-template-areas:
+      'img title status cancel'
+      'img info status cancel'
+      'img type status cancel';
+    grid-template-columns: 23% 47% 15% 15%;
+    margin: 0 auto;
+    /* text-align: center; */
+    border-top: 1px solid ${Colors.lightGray};
+    padding: .4rem 1rem;
+    width: 40rem;
+  }
+  .dogwalker-img {
+    grid-area: img;
+    width: 7.5rem;
+    height: 7.5rem;
+    border: 0.5px solid rgb(238, 238, 238);
+    object-fit: cover;
+  }
+  .name {
+    grid-area: title;
+  }
+  .info {
+    grid-area: info;
+  }
+  .type {
+    grid-area: type;
+  }
+  .status {
+    grid-area: status;
+    display: flex;
+    align-self: center;
+    align-items: center;
+    justify-content: center;
+    height: 5rem;
+    border: 1px solid ${Colors.mediumGray};
+    border-radius: 4px;
+  }
+  .cancel {
+    cursor: pointer;
+    grid-area: cancel;
+    display: flex;
+    align-self: center;
+    align-items: center;
+    justify-content: center;
+    /* background-color: navajowhite; */
+  }
+`
 
 function MyRequest () {
   const dispatch = useDispatch();
@@ -30,7 +83,19 @@ function MyRequest () {
     <MyRequestWrapper>
       <TopNavigation />
       <div className='main'>
-        My Request
+        <div className='container'>
+          {requestList.map((el, idx) => {
+            return (
+              <div className='card' key={idx}>
+                <img className='dogwalker-img' src={el.img} alt={el.name} />
+                <div className='name'>{el.name}</div>
+                  <div className='info'>{el.date} <span>|</span> {el.duration}분 / {el.price}원</div>
+                <div className='type'>{el.type}</div>
+                <div className='status'>{el.status}</div>
+                <div className='cancel'>요청 취소</div>
+              </div>
+          )})}
+        </div>
       </div>
     </MyRequestWrapper>
   );
