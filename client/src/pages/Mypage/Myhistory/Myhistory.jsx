70510b3eccfe7843fe1c338e2eb7a4b464116c10
Kyungjoo Ha <429why@gmail.com>
2021-11-12 14:24:14 +0900

    add: Basic contents

--- client/src/pages/Mypage/Myhistory/Myhistory.jsx
+++ client/src/pages/Mypage/Myhistory/Myhistory.jsx
@@ -1,6 +1,8 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { cancelRating } from '../../../redux/action';
 import { useState } from 'react';
 import styled from 'styled-components';
+import { Colors } from '../../../components/utils/_var';
 import TopNavigation from '../../../components/TopNavigation';
 import Rating from './Rating';
 import Review from './Review';
@@ -9,6 +11,49 @@ export const MyHistoryWrapper = styled.div`
   .main {
     display: flex;
     min-height: calc(100vh - 10.9rem);
+  }
+  .container {
+    margin: 1rem auto;
+  }
+  .card {
+    display: grid;
+    grid-template-areas:
+      'img title rating review'
+      'img info rating review'
+      'img type rating review';
+    grid-template-columns: 23% 47% 15% 15%;
+    margin: 0 auto;
+    /* text-align: center; */
+    border-top: 1px solid ${Colors.lightGray};
+    padding: .4rem 1rem;
+    width: 40rem;
+  }
+  .dogwalker-img {
+    grid-area: img;
+    width: 7.5rem;
+    height: 7.5rem;
+    border: 0.5px solid rgb(238, 238, 238);
+    object-fit: cover;
+  }
+  .name {
+    grid-area: title;
+  }
+  .info {
+    grid-area: info;
+  }
+  .type {
+    grid-area: type;
+  }
+  .rating {
+    grid-area: rating;
+    background-color: honeydew;
+  }
+  .review {
+    grid-area: review;
+    background-color: mediumturquoise;
+  }
+  .bnt {
+    cursor: pointer;
   }
 `;
 
@@ -25,9 +70,12 @@ function MyHistory () {
   const historyList = useSelector((state) => state.history).dogWalkerHistory;
   const [openRating, setOpenRating] = useState(false);
   const [openReview, setOpenReview] = useState(false);
+  const [walkerId, setWalkerId] = useState(0);
 
-  console.log(historyList);
-  const handleRatingOpen = () => {
+  // console.log(historyList);
+
+  const handleRatingOpen = (id) => {
+    setWalkerId(id);
     setOpenRating(true);
   };
 
@@ -35,23 +83,39 @@ function MyHistory () {
     setOpenRating(false);
   };
 
-  const handleReviewOpen = () => {
+  const handleReviewOpen = (id) => {
+    setWalkerId(id);
     setOpenReview(true);
   };
 
   const handleReviewClose = () => {
     setOpenReview(false);
   };
-  
+
+  const handleCancel = () => {
+    dispatch(cancelRating(1, 5));
+  };
+
   return (
     <MyHistoryWrapper>
       <TopNavigation />
       <div className='main'>
-        My History
-        <div onClick={handleRatingOpen}>평점 등록</div>
-        <div onClick={handleReviewOpen}>리뷰 등록</div>
-        {openRating ? <Rating handleModal={handleRatingClose} /> : null}
-        {openReview ? <Review handleModal={handleReviewClose} /> : null}
+        <div className='container'>
+          {historyList.map((el, idx) => {
+            return (
+              <div className='card' key={idx}>
+                <img className='dogwalker-img' src={el.img} alt={el.name} />
+                <div className='name'>{el.name}</div>
+                <div className='info'>{el.date} <span>|</span> {el.duration}분 / {el.price}원</div>
+                <div className='type'>{el.type}</div>
+                <div className='bnt rating' onClick={() => handleRatingOpen(el.dogwalkerId)}>평점 등록</div>
+                <div className='bnt review' onClick={() => handleReviewOpen(el.dogwalkerId)}>리뷰 등록</div>
+              </div>
+            );
+          })}
+        </div>
+        {openRating ? <Rating handleModal={handleRatingClose} dogwalkerId={walkerId} /> : null}
+        {openReview ? <Review handleModal={handleReviewClose} dogwalkerId={walkerId} /> : null}
       </div>
     </MyHistoryWrapper>
   );
