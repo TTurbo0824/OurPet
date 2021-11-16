b04abc163ccb792b62fdd507ab0dbbc95f0c5eb1
Kyungjoo Ha <429why@gmail.com>
2021-11-16 17:50:04 +0900

    add: Review actions

--- client/src/redux/reducers/review.js
+++ client/src/redux/reducers/review.js
@@ -1,12 +1,37 @@
-import { POST_REVIEW, DELETE_REVIEW } from '../action';
+import { POST_REVIEW, DELETE_REVIEW, TRACK_REVIEW } from '../action';
 import { initReviewState } from './initialState/initReviewState';
 
 function review (state = initReviewState, action) {
   switch (action.type) {
     case POST_REVIEW:
-      return { ...state };
+      return {
+        ...state,
+        dogWalkers: state.dogWalkers.map((el) =>
+          el.id === action.payload.id
+            ? {
+                ...el,
+                review: [
+                  ...el.review,
+                  { nickname: action.payload.nickname, content: action.payload.content, date: action.payload.date }
+                ]
+              }
+            : el
+        )
+      };
     case DELETE_REVIEW:
-      return { ...state };
+      return {
+        ...state,
+        dogWalkers: state.dogWalkers.map((el) =>
+          el.id === action.payload.id
+            ? { ...el, review: el.review.filter((_, idx) => idx !== action.payload.idx) }
+            : el
+        )
+      };
+    case TRACK_REVIEW:
+      return {
+        ...state,
+        givenReview: [...state.givenReview, action.payload]
+      };
     default:
       return state;
   }
