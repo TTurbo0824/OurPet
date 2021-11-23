5f44fc9a4572ae3387af9a8b971898fa96fceb3a
Kyungjoo Ha <429why@gmail.com>
2021-11-23 23:21:32 +0900

    modify: Datepicker CSS

--- client/src/pages/Dogwalker/DateSelector.jsx
+++ client/src/pages/Dogwalker/DateSelector.jsx
@@ -1,20 +1,23 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendar } from '@fortawesome/free-regular-svg-icons';
-import DatePicker from 'react-datepicker';
-import { Colors } from '../../components/utils/_var';
+import { useState } from 'react';
 import styled from 'styled-components';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { Colors } from '../../components/utils/_var';
+import { media } from '../../components/utils/_media';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { ko } from 'date-fns/esm/locale';
 
 const DatePickerWrapper = styled.div`
   input {
     border: none;
     width: 100%;
-    margin: 0 0.4rem 0.25rem;
     color: ${Colors.gray};
     font-size: 1rem;
+    /* background-color: lightseagreen; */
   }
   .description {
-    margin-bottom: .8rem;
+    margin-bottom: 0.8rem;
   }
 `;
 
@@ -24,10 +27,27 @@ const DateContainer = styled.div`
   align-items: center;
   width: 100%;
   height: 2.35rem;
-  padding: 0 0.4rem;
+  padding: 0 0.7rem;
   background-color: white;
   border: 1px solid ${Colors.lightGray};
   border-radius: 4px;
+  .date-customheader {
+    display: flex;
+    justify-content: center;
+    margin: .5rem auto;
+  }
+  .nav-button {
+    border: none;
+    background-color: white;
+    cursor: pointer;
+  }
+  .nav-icon {
+    margin: 0 1.5rem;
+  }
+  .custom-heading {
+    font-size: .9rem;
+    font-weight: bold;
+  }
 `;
 
 function DateSelector ({ requestOptions, setRequestOptions }) {
@@ -42,13 +62,41 @@ function DateSelector ({ requestOptions, setRequestOptions }) {
     <DatePickerWrapper>
       <div className='description'>언제 산책을 원하시나요?</div>
       <DateContainer>
-        <FontAwesomeIcon icon={faCalendar} color={Colors.darkGray} size='1x' />
         <DatePicker
+          locale={ko}
+          showPopperArrow={false}
           selected={startDate}
           dateFormat='yyyy.MM.dd'
           onSelect={(date) => setStartDate(date)}
           onChange={(date) => dateSelected(date)}
           minDate={new Date()}
+          renderCustomHeader={({
+            date,
+            decreaseMonth,
+            increaseMonth,
+            prevMonthButtonDisabled,
+            nextMonthButtonDisabled
+          }) => (
+            <div className='date-customheader'>
+              <button
+                className='nav-button'
+                onClick={decreaseMonth}
+                disabled={prevMonthButtonDisabled}
+              >
+                <FontAwesomeIcon className='nav-icon' icon={faChevronLeft} size='1x' />
+              </button>
+              <div className='custom-heading'>
+                {date.getFullYear()}년 {date.getMonth() + 1}월
+              </div>
+              <button
+                className='nav-button'
+                onClick={increaseMonth}
+                disabled={nextMonthButtonDisabled}
+              >
+                <FontAwesomeIcon className='nav-icon' icon={faChevronRight} size='1x' />
+              </button>
+            </div>
+          )}
         />
       </DateContainer>
     </DatePickerWrapper>
--- client/src/pages/Mainpage/DateSelector.jsx
+++ client/src/pages/Mainpage/DateSelector.jsx
@@ -1,30 +1,46 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendar } from '@fortawesome/free-regular-svg-icons';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { Colors } from '../../components/utils/_var';
 import { media } from '../../components/utils/_media';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { ko } from 'date-fns/esm/locale';
 
 const DateContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  /* width: 21rem; */
   width: 80%;
   ${media.tablet`width: 21rem;`}
   ${media.laptop`width: 28rem;`}
   height: 2.35rem;
-  padding: 0 1rem;
+  padding: 0 0.7rem;
   font-size: 1.1rem;
   background-color: white;
   border: 1px solid ${Colors.mediumGray};
   border-radius: 4px;
-
   input {
     font-size: 1rem;
     color: ${Colors.gray};
+  }
+  .date-customheader {
+    display: flex;
+    justify-content: center;
+    margin: .5rem auto;
+  }
+  .nav-button {
+    border: none;
+    background-color: white;
+    cursor: pointer;
+  }
+  .nav-icon {
+    margin: 0 1.5rem;
+  }
+  .custom-heading {
+    font-size: .9rem;
+    font-weight: bold;
   }
 `;
 
@@ -33,16 +49,32 @@ function DateSelector () {
 
   return (
     <DateContainer>
-      <FontAwesomeIcon
-        style={{ marginRight: '.3rem' }}
-        icon={faCalendar}
-        color={Colors.darkGray}
-        size='1x'
-      />
       <DatePicker
+        locale={ko}
+        showPopperArrow={false}
         selected={startDate}
         dateFormat='yyyy.MM.dd'
         onChange={(date) => setStartDate(date)}
+        minDate={new Date()}
+        renderCustomHeader={({
+          date,
+          decreaseMonth,
+          increaseMonth,
+          prevMonthButtonDisabled,
+          nextMonthButtonDisabled
+        }) => (
+          <div className='date-customheader'>
+            <button className='nav-button' onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+              <FontAwesomeIcon className='nav-icon' icon={faChevronLeft} size='1x' />
+            </button>
+            <div className='custom-heading'>
+              {date.getFullYear()}년 {date.getMonth() + 1}월
+            </div>
+            <button className='nav-button' onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+              <FontAwesomeIcon className='nav-icon' icon={faChevronRight} size='1x' />
+            </button>
+          </div>
+        )}
       />
     </DateContainer>
   );
