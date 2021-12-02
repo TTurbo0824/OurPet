c7ab2c0e238b765a5f55fc59776e2b8b3a6319d8
Kyungjoo Ha <429why@gmail.com>
2021-12-02 23:41:30 +0900

    add: Content

--- client/src/pages/Landing/LadingPage.jsx
+++ client/src/pages/Landing/LadingPage.jsx
@@ -6,6 +6,8 @@ import corgi from '../../images/corgi.jpeg';
 import default_profile from '../../images/default_profile.jpeg';
 import { Colors } from '../../components/utils/_var';
 import { media } from '../../components/utils/_media-queries';
+import charge01 from '../../images/charge01.jpeg';
+import charge02 from '../../images/charge02.jpeg';
 
 const LandingWrapper = styled.div`
   .main {
@@ -18,33 +20,65 @@ const LandingWrapper = styled.div`
     background-color: lavender;
     width: 100vw;
     min-height: 35rem;
+    padding: 1rem;
     margin-bottom: 1rem;
+  }
+  .title {
+    text-align: center;
+    margin: 1rem auto 2.5rem;
+    font-size: 2.75rem;
   }
   .review-container {
     display: flex;
     justify-content: space-around;
-    background-color: pink;
+    /* background-color: pink; */
     width: 15rem;
     flex-wrap: wrap;
     ${media.tablet`width: 92vw;`}
     margin: 0 auto;
   }
-  .review-card {
+  .price-container {
+    display: flex;
+
+  }
+  .price-card {
     /* border: 1px solid black; */
-    width: 15rem;
-    padding: 1rem;
+    width: 16.5rem;
+    height: 25rem;
     background-color: white;
     border-radius: 3px;
+    box-shadow: 1px 1px 5px 2px ${Colors.lightGray};
+  }
+
+  .review-card {
+    /* border: 1px solid black; */
+    width: 16.5rem;
+    background-color: white;
+    border-radius: 3px;
+    box-shadow: 1px 1px 5px 2px ${Colors.lightGray};
   }
   .profile-container {
-    width: 12rem;
-    height: 8rem;
+    width: 15rem;
+    height: 10rem;
     overflow: hidden;
     position: relative;
     margin: auto;
+    margin-top: .5rem;
   }
   .review-profile {
-    width: 12rem;
+    width: 15rem;
+  }
+  .nickname {
+    font-size: 1.2rem;
+    margin: .5rem 0 1rem ;
+    color: ${Colors.black};
+    padding: .5rem 1.5rem 0;
+  }
+  .content {
+    color: ${Colors.darkGray};
+    line-height: 1.65rem;
+    font-size: .95rem;
+    padding: 0 1.5rem 1.5rem;
   }
 `;
 
@@ -53,7 +87,7 @@ function LandingPage () {
   let selectedReview = [...reviews];
   selectedReview = selectedReview.slice(0, 3);
   // console.log(selectedReview);
-
+  const charges = [{ duration: '30분 산책', price: '₩12,000', img: charge01 }, { duration: '60분 산책', price: '₩15,000', img: charge02 }];
   const profileImage = [pug, minipin, corgi, default_profile];
 
   return (
@@ -63,9 +97,21 @@ function LandingPage () {
           grand introduction
           <button>내 주변 도그워커 찾기</button>
         </div>
-        <div className='landing-container'>service introduction (price/duration)</div>
         <div className='landing-container'>
-          방문 도그워커 후기
+          우리 아이 산책, 워킹도그에게 맡겨보세요
+          <div className='price-container'>
+            {charges.map((el, idx) => (
+              <div key={idx}>
+                <div className='price-card'>
+                  <img className='review-profile' alt='profile' src={el.img} />
+                  {el.duration} {el.price}부터
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className='landing-container'>
+          <div className='title'>워킹도그<br />방문 도그워커 후기</div>
           <div className='review-container'>
             {selectedReview.map((review, idx) => (
               <div className='review-card' key={idx}>
