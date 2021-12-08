b501a07f2cf28ac39bea91d1426f5eaaba2e57db
Kyungjoo Ha <429why@gmail.com>
2021-12-08 20:58:26 +0900

    modify: Controller for deleting the review

--- server/controllers/history/delete-review.js
+++ server/controllers/history/delete-review.js
@@ -5,25 +5,26 @@ module.exports = async (req, res) => {
   try {
     // JUST FOR TESTING PURPOSES
     // console.log(req.headers.authorization);
-    const accessTokenData = { id: req.headers.authorization };
-    // const accessTokenData = isAuthorized(req);
+    // const accessTokenData = { id: req.headers.authorization };
+    const accessTokenData = isAuthorized(req);
 
     if (!accessTokenData) {
       return res.status(401).json({ message: 'You\'re not logged in' });
     } else {
-      const { historyId } = req.body;
+      const { id } = req.body;
+
       const userReview = await reviews.findAll({
         where: {
-          historyId: historyId
+          id: id
         }
       });
 
       if (userReview.length === 0) {
-        return res.status(409).json({ message: 'review is already deleted' });
+        return res.status(404).json({ message: 'Review not found' });
       } else {
         await reviews.destroy({
           where: {
-            id: historyId
+            id: id
           }
         });
 
