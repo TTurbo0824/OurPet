d5104d7c9aef6fd621f36d684ee5fde7a0a5ce76
Kyungjoo Ha <429why@gmail.com>
2021-12-17 14:59:35 +0900

    add: New redux action

--- client/src/redux/reducers/rating.js
+++ client/src/redux/reducers/rating.js
@@ -1,4 +1,4 @@
-import { GET_RATING, GIVE_RATING, REMOVE_RATING } from '../action';
+import { GET_RATING, GIVE_RATING, EDIT_RATING, REMOVE_RATING } from '../action';
 import { initRatingState } from './initialState/initRatingState';
 
 function rating (state = initRatingState, action) {
@@ -12,6 +12,14 @@ function rating (state = initRatingState, action) {
       return {
         ...state,
         givenRating: [...state.givenRating, action.payload]
+      };
+    case EDIT_RATING:
+      return {
+        ...state,
+        givenRating: state.givenRating.map((el) => {
+          if (el.id === action.payload.id) el.rating = action.payload.rating;
+          return el;
+        })
       };
     case REMOVE_RATING:
       return {
