384a8ce6eb4d2158301ed6514f7d1f4367f82f9e
Kyungjoo Ha <429why@gmail.com>
2021-12-17 14:25:42 +0900

    add: Controller for editing the given rating

---
+++ server/controllers/history/edit-rating.js
@@ -0,0 +1,37 @@
+const { ratings } = require('../../models');
+const { isAuthorized } = require('../tokenFunctions');
+
+module.exports = async (req, res) => {
+  try {
+    // JUST FOR TESTING PURPOSES
+    // console.log(req.headers.authorization);
+    // const accessTokenData = { id: req.headers.authorization };
+    const accessTokenData = isAuthorized(req);
+
+    if (!accessTokenData) {
+      return res.status(401).json({ message: 'You\'re not logged in' });
+    } else {
+      const { id, rating } = req.body;
+      const userReview = await ratings.findAll({
+        where: {
+          historyId: id
+        }
+      });
+
+      if (userReview.length === 0) {
+        return res.status(404).json({ message: 'Rating not found' });
+      } else {
+        await ratings.update({
+          rating: rating
+        },
+        {
+          where: { historyId: id }
+        });
+
+        return res.status(200).json({ message: 'ok' });
+      }
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'error' });
+  }
+};
