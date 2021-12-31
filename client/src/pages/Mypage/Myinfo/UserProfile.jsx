7c382251bc9ed82fca15507df870d46079d7992b
Kyungjoo Ha <429why@gmail.com>
2021-12-31 12:47:42 +0900

    feat: Profile picture limits

--- client/src/pages/Mypage/Myinfo/UserProfile.jsx
+++ client/src/pages/Mypage/Myinfo/UserProfile.jsx
@@ -6,11 +6,11 @@ import styled from 'styled-components';
 import { Colors } from '../../../components/utils/_var';
 import default_profile from '../../../images/default_profile.jpeg';
 import { HistoryButton, ProfileImage, ReviewView } from '../../../components/MyPageComponents';
-import { Backdrop } from '../../../components/UserComponents';
+import { Alertbox, Backdrop } from '../../../components/UserComponents';
 import CloseButton from '../../../components/CloseButton';
 
 const ProfileImageWrapper = styled.div`
-  margin: .75rem 0;
+  margin: .7rem 0;
   .loading {
     width: 5rem;
     margin-top: 2.25rem;
@@ -21,17 +21,15 @@ const UploadLabel = styled.label`
   cursor: pointer;
   display: block;
   font-size: .85rem;
-  margin: .3rem auto .8rem;
+  margin: .3rem auto;
   color: ${Colors.darkGray};
   text-decoration: underline;
 `;
 
 function UserProfile ({ profile_url, token, handleMessage, handleNotice, modal, handleProfileClose }) {
   const dispatch = useDispatch();
-
-  console.log(profile_url);
   const [isLoading, setIsLoading] = useState(false);
-
+  const [errorMsg, setErrorMsg] = useState('');
   const [files, setFiles] = useState({
     file: '',
     previewURL: ''
@@ -39,9 +37,18 @@ function UserProfile ({ profile_url, token, handleMessage, handleNotice, modal, 
 
   const onLoadFile = (e) => {
     e.preventDefault();
-
     const reader = new FileReader();
     const file = e.target.files[0];
+    const maxSize = 5 * 1024 * 1024;
+
+    if (file.size && file.size > maxSize) {
+      setErrorMsg('5MB 이내의 이미지만 등록 가능합니다.');
+    } else if (file.type && !file.type.includes('image')) {
+      setErrorMsg('이미지만 업로드 가능합니다.');
+    } else {
+      setErrorMsg('');
+    }
+
     reader.onloadend = () => {
       setFiles({
         file: file,
@@ -58,42 +65,42 @@ function UserProfile ({ profile_url, token, handleMessage, handleNotice, modal, 
 
   const onSubmit = (e) => {
     e.preventDefault();
-    const formData = new FormData();
-    formData.append('image', files.file);
-    setIsLoading(true);
-    axios
-      .patch(`${process.env.REACT_APP_API_URL}/profile-image`, formData, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json'
-        }
-      })
-      .then((res) => {
-        const { url } = res.data.data;
-        // console.log(res.data);
-        setIsLoading(false);
-        setFiles({ ...files, previewURL: url });
-        dispatch(editProfile(url));
-        handleProfileClose();
-        handleNotice(true);
-        handleMessage('프로필 사진이 수정되었습니다.');
-      })
-      .catch((error) => {
-        if (error.response.status === 401) {
-          modal();
-        } else {
+    if (files.previewURL === '') {
+      setErrorMsg('수정할 이미지를 선택해주세요.');
+    } else if (!errorMsg.includes('이미지만')) {
+      const formData = new FormData();
+      formData.append('image', files.file);
+      setIsLoading(true);
+      axios
+        .patch(`${process.env.REACT_APP_API_URL}/profile-image`, formData, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json'
+          }
+        })
+        .then((res) => {
+          const { url } = res.data.data;
+
           setIsLoading(false);
+          setFiles({ ...files, previewURL: url });
+          dispatch(editProfile(url));
           handleProfileClose();
           handleNotice(true);
-          handleMessage('오류가 발생하였습니다.');
-          console.log('error: ', error.response.data.message);
-        }
-      });
+          handleMessage('프로필 사진이 수정되었습니다.');
+        })
+        .catch((error) => {
+          if (error.response.status === 401) {
+            modal();
+          } else {
+            setIsLoading(false);
+            handleProfileClose();
+            handleNotice(true);
+            handleMessage('오류가 발생하였습니다.');
+            console.log('error: ', error.response.data.message);
+          }
+        });
+    }
   };
-
-  // console.log(files);
-
-  console.log(isLoading);
 
   return (
     <Backdrop>
@@ -113,6 +120,7 @@ function UserProfile ({ profile_url, token, handleMessage, handleNotice, modal, 
             <HistoryButton bntColor={Colors.lightYellow} type='submit'>사진 수정</HistoryButton>
             <HistoryButton bntColor={Colors.gray} onClick={handleProfileClose}>취소</HistoryButton>
           </form>
+          <Alertbox>{errorMsg}</Alertbox>
         </ProfileImageWrapper>
       </ReviewView>
     </Backdrop>
