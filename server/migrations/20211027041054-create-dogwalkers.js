0f285a40231ae8004dbdd7b0b42883b44a9c450f
Kyungjoo Ha <429why@gmail.com>
2022-01-04 19:07:24 +0900

    delete: Unused column

--- server/migrations/20211027041054-create-dogwalkers.js
+++ server/migrations/20211027041054-create-dogwalkers.js
@@ -11,9 +11,6 @@ module.exports = {
       name: {
         type: Sequelize.STRING
       },
-      profile: {
-        type: Sequelize.TEXT
-      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
